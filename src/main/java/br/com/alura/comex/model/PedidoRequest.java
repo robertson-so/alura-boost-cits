b3a20f5da585cb22a7ee0fdcdf849b76e2f92e49
Robertson S. O <robertson.so@gmail.com>
2022-10-26 08:00:59 -0300

    feat: adding annotation to use custom validation for an order request.

--- src/main/java/br/com/alura/comex/model/PedidoRequest.java
+++ src/main/java/br/com/alura/comex/model/PedidoRequest.java
@@ -1,11 +1,13 @@
 package br.com.alura.comex.model;
 
+import br.com.alura.comex.validator.ValidPedido;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@ValidPedido
 public class PedidoRequest {
 
   @NotNull
