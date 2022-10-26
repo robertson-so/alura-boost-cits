ead1b3a5dda1a9955ed30ec1d4ccfebbff840f80
Robertson S. O <robertson.so@gmail.com>
2022-10-25 22:09:54 -0300

    Adding repositories.

---
+++ src/main/java/br/com/alura/comex/repository/ClienteRepository.java
@@ -0,0 +1,23 @@
+package br.com.alura.comex.repository;
+
+import br.com.alura.comex.model.Cliente;
+import br.com.alura.comex.model.ClienteProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+  @Query(value = """
+      select cli.nome,
+      cli.cpf,
+      cli.telefone,
+      concat(cli.cidade, '/', cli.estado) as local
+      from Cliente cli
+      """)
+  Page<ClienteProjection> findAllProjecao(Pageable pageable);
+
+}
---
+++ src/main/java/br/com/alura/comex/repository/PedidoRepository.java
@@ -0,0 +1,28 @@
+package br.com.alura.comex.repository;
+
+import br.com.alura.comex.model.Pedido;
+import br.com.alura.comex.model.PedidoCategoriaProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends CrudRepository<Pedido, Long> {
+
+  @Query(value = """
+      select cat.nome as categoria,
+      (select sum(item.quantidade)
+       from ItemDePedido item
+       left join item.produto prod
+       where prod.categoria = cat) as quantidadeProdutosVendidos,
+      (select sum(item.precoUnitario * item.quantidade)
+       from ItemDePedido item
+       left join item.produto prod
+       where prod.categoria = cat) as montanteVendido
+      from Categoria cat
+      order by cat.nome
+      """)
+  Page<PedidoCategoriaProjection> findVendidos(Pageable pageable);
+}
