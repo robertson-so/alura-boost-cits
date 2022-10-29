b848d0976b9667337c7fc40625111e42fb8f7862
Robertson S. O <robertson.so@gmail.com>
2022-10-28 22:59:10 -0300

    refactor: adding id to the projection.

--- src/main/java/br/com/alura/comex/controller/domain/ClienteProjectionResponse.java
+++ src/main/java/br/com/alura/comex/controller/domain/ClienteProjectionResponse.java
@@ -4,16 +4,22 @@ import br.com.alura.comex.model.ClienteProjection;
 
 public final class ClienteProjectionResponse {
 
+  private final Long id;
   private final String nome;
   private final String cpf;
   private final String telefone;
   private final String local;
 
   public ClienteProjectionResponse(ClienteProjection clienteProjection) {
+    this.id = clienteProjection.getId();
     this.nome = clienteProjection.getNome();
     this.cpf = clienteProjection.getCPF();
     this.telefone = clienteProjection.getTelefone();
     this.local = clienteProjection.getLocal();
+  }
+
+  public Long getId() {
+    return id;
   }
 
   public String getNome() {
